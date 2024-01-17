370c92b4c87b8d12eaaab6d32b9797a454fa880c
MIRMOSTAFA-PC\Mirmostafa <mirmostafa@hotmail.com>
2024-01-17 10:38:20 +0330

    initial commit

---
+++ BackgroundServices/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace BackgroundServices;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
