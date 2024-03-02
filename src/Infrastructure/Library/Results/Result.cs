bc83b29f055fa739434426e3d9871a5706f96a40
mirmostafa <mirmostafa@hotmail.com>
2024-03-02 12:58:15 +0330

    Some code cleanup

--- src/Infrastructure/Library/Results/Result.cs
+++ src/Infrastructure/Library/Results/Result.cs
@@ -1,65 +1,85 @@
-﻿using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
+﻿using Moshaveran.Library.Results.Internals;
 
-using Moshaveran.Library.Results.Internals;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Moshaveran.Library.Results;
 
-[DebuggerStepThrough]
-[StackTraceHidden]
+[DebuggerStepThrough, StackTraceHidden]
 public sealed class Result : ResultBase, IResult
 {
-    private static Result? _failed;
+    private static IResult? _failed;
 
-    private static Result? _succeed;
+    private static IResult? _succeed;
 
     internal Result(bool isSucceed, string? message = null, params Exception[] exceptions) : base(isSucceed, message, exceptions)
     {
     }
 
-    internal Result(bool isSucceed, string? message = null, IEnumerable<Exception>? exceptions = null) : base(isSucceed, message, exceptions)
+    internal Result(bool isSucceed, string? message = null, IEnumerable<Exception>? exceptions = null)
+        : this(isSucceed, message, exceptions?.ToArray() ?? [])
     {
     }
 
     [NotNull]
-    public static Result Failed => _failed ??= new(false);
+    public static IResult Failed => _failed ??= Fail();
 
     [NotNull]
-    public static Result Succeed => _succeed ??= new(true);
+    public static IResult Succeed => _succeed ??= Success();
 
     [return: NotNull]
-    public static Result Create(bool isSucceed)
-        => new(isSucceed);
+    public static IResult Create(bool isSucceed)
+        => new Result(isSucceed);
 
     [return: NotNull]
-    public static Result Create(bool isSucceed, string? message)
-        => new(isSucceed, message);
+    public static IResult Create(bool isSucceed, string? message)
+        => new Result(isSucceed, message);
 
     [return: NotNull]
-    public static Result Create(Exception exception)
-        => new(false, exceptions: exception);
+    public static IResult Create(Exception exception)
+        => new Result(false, exceptions: exception);
 
     [return: NotNull]
-    public static Result<TValue> Create<TValue>(TValue value, bool isSucceed)
-        => new(value, isSucceed);
+    public static IResult<TValue> Create<TValue>(TValue value, bool isSucceed)
+        => new Result<TValue>(value, isSucceed);
 
     [return: NotNull]
-    public static Result<TValue> Create<TValue>(TValue value, bool isSucceed, string? message)
-        => new(value, isSucceed, message);
+    public static IResult<TValue> Create<TValue>(TValue value, bool isSucceed, string? message)
+        => new Result<TValue>(value, isSucceed, message);
 
     [return: NotNull]
-    public static Result<TValue> Fail<TValue>(TValue value)
-        => new(value, false);
+    public static IResult<TValue> Fail<TValue>(TValue value)
+        => new Result<TValue>(value, false);
 
     [return: NotNull]
-    public static Result Fail(Exception exception)
-        => new(false, exceptions: exception);
+    public static IResult Fail()
+        => new Result(false);
 
     [return: NotNull]
-    public static Result<TValue> Fail<TValue>(TValue value, string message)
-        => new(value, false, message);
+    public static IResult Fail(string message)
+        => new Result(false, message: message);
 
     [return: NotNull]
-    public static Result<TValue> Success<TValue>(TValue value)
-        => new(value, true);
+    public static IResult Fail(Exception exception)
+        => new Result(false, exceptions: exception);
+
+    [return: NotNull]
+    public static IResult<TValue> Fail<TValue>(TValue value, string message)
+        => new Result<TValue>(value, false, message);
+
+    [return: NotNull]
+    public static IResult Success(string message)
+        => new Result(true, message: message);
+
+    [return: NotNull]
+    public static IResult Success()
+        => new Result(true);
+
+    [return: NotNull]
+    public static IResult<TValue> Success<TValue>(TValue value, string message)
+        => new Result<TValue>(value, true, message: message);
+
+    [return: NotNull]
+    public static IResult<TValue> Success<TValue>(TValue value)
+        => new Result<TValue>(value, true);
 }
