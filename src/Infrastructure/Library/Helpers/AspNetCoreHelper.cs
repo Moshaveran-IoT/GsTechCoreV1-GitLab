c4822e65bf9f26339f70033796c40c323744db63
mirmostafa <mirmostafa@hotmail.com>
2024-04-02 14:28:32 +0330

    Closes #174 , Close #175

---
+++ src/Infrastructure/Library/Helpers/AspNetCoreHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moshaveran.Library.Helpers;
+public static class AspNetCoreHelper
+{
+    public static string? GetEnvironment()
+    {
+        return Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+    }
+}
